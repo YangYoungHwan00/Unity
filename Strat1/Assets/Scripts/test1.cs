dd095f96cbc9bf86228370269dce75aebb83a53d
YangYoungHwan00 <qjddldi17@gmail.com>
2024-01-31 19:30:43 +0900

    delete .DS_Store

--- Strat1/Assets/Scripts/test1.cs
+++ Strat1/Assets/Scripts/test1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,6 @@ public class test1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
